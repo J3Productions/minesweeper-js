68585546c447b78518e27c7515daa2f639646911
Jacob Parnell <jparnell1997@gmail.com>
2018-09-15 23:50:32 -0500

    Finished documentation in main.js, cited sources in README.md

--- js/main.js
+++ js/main.js
@@ -7,15 +7,11 @@ import { Board } from './board.js'
  */
 let game = undefined;
 
-//Date: 09/06/2018
-//Version 1, Author: Jielong Cong
-
-//Date: 09/07/2018
-//Version 2, Author: Jason Purinton
-
-//Date: 09/07/2018
-//Version 3, Author: Jielong Cong
-
+/**
+ * Run when length input and height field are changed, enables and disables the number of mines input field based on validity of length and height input field. Checks length and height input fields and checks whether they are valid or not. If they are both valid (non-empty and within the range 2-45), the input field for the number of mines is set to non-read only and the maximum value is set to be equal to the area of the board - 1. If not, the input field for the number of mines is set to read only.
+ * @pre boardLength and boardHeight have been changed by the user and both are valid inputs.
+ * @post minesInput's readOnly attribute has been set to true or false.
+ */
 export function checkMinesInput() {
     let lengthField = document.getElementById("boardLength");
     let heightField = document.getElementById("boardHeight");
@@ -30,6 +26,11 @@ export function checkMinesInput() {
     }
 }
 
+/**
+ * Run when start/restart button is pressed, generates a game board if input conditions are met. Checks the validitiy of every input field (non-empty and within their respective ranges). If they are all valid, a new {@link Board} object is created and stored in game. The Board then plants the mines and all adjacent numbers calling {@link plantMine} and {@link plantAdjNum}. The game board area is then cleared and a table is created by creating element nodes of type TD, attaching a numeric ID to each one. These are then appended into TR nodes, which are then appended to the TABLE node in index.html. The status display is then unhidden and the start button is changed to a reset button. If all input fields are not valid, display an alert and force the user to try their inputs again.
+ * @pre startButton has been clicked by the user and boardLength, boardHeight, and minesInput are valid inputs.
+ * @post game is now a Board with mines and adjacent numbers planted, game is represented as a table, startButton is now red and reads Restart, and flags/mines stat bar is now shown.
+ */
 export function boardGen() {
 	let lengthField = document.getElementById("boardLength");
 	let heightField = document.getElementById("boardHeight");
@@ -71,6 +72,13 @@ export function boardGen() {
 	}
 }
 
+/**
+ * Run when any tile in the game table is clicked, reveals the tile clicked on. Checks if tile clicked on is unrevealed and if the game is still happening. If not, return null. If so, run {@link clickreveal} on the tile, determining its coordinates from the tile's ID. If the tile is a mine, run {@link showAllMine}, display all mines on the board and show the game over display. If the tile is a number not equal to 0, show the number on the clicked tile. If the tile is equal to 0, loop through the whole board looking for revealed tiles and display all revealed tiles.
+ * @param {Object} clicked The TD object that was clicked, gotten from event.target
+ * @pre A tile on the game board has been left-clicked by the user, the game is still running, the tile clicked on has not yet been revealed
+ * @post The tile clicked on has been revealed, as well as any adjacent tiles if the tile clicked on is a 0
+ * @return {null} Return null only if the game is over or the tile has already been revealed.
+ */
 export function leftClick(clicked) {
 	let cellID = Number(clicked.getAttribute("id"));
 	let flags = document.getElementById("flagsPlaced").innerHTML;
@@ -129,6 +137,13 @@ export function leftClick(clicked) {
 	document.getElementById("flagsPlaced").innerHTML = flags;
 }
 
+/**
+ * Run when any tile in the game table is right-clicked, places or removes a flag on the tile clicked. Checks if if the game is still happening. If not, return null. If so, determine coordinates of the tile clicked using its ID and execute {@link flagTile}. If {@link flagTile} returns true, change the space inside the tile to be a flag. If {@link flagTile} returns false, remove the flag inside the tile. If {@link flagTile} returns null, return null. Then check if the game was won (all flags placed on all mines). If so, show the winner display.
+ * @param {Object} clicked The TD object that was clicked, gotten from event.target
+ * @pre A tile on the game board has been right-clicked by the user, the game is still running, the tile clicked on has not yet been revealed
+ * @post The tile clicked on has been flagged if not already, the flag is removed if it was already flagged.
+ * @return {null} Return null only if the game is over or the tile has already been revealed.
+ */
 export function rightClick(clicked) {
 	let cellID = Number(clicked.getAttribute("id"));
 	let flags = document.getElementById("flagsPlaced").innerHTML;
